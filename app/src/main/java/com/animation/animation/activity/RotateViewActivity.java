f2ba2826cdef510fd29324fe879cf26d36fd741d
xianjuren <renxianju@simiao-internet.com>
2017-04-21 20:03:56 +0800

    add code

--- app/src/main/java/com/animation/animation/activity/RotateViewActivity.java
+++ app/src/main/java/com/animation/animation/activity/RotateViewActivity.java
@@ -5,15 +5,16 @@ import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.CycleInterpolator;
+import android.view.animation.BounceInterpolator;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 
 import com.animation.animation.R;
+import com.animation.animation.util.Rotate3dAnimation;
 
-import static android.view.animation.Animation.INFINITE;
-import static android.view.animation.Animation.RESTART;
 
 /**
  * Created by Jone on 17/4/20.
@@ -21,21 +22,39 @@ import static android.view.animation.Animation.RESTART;
 public class RotateViewActivity extends BaseBackActivity {
 
     ImageView mCircleBg, mWindmill;
-    Animation mCircleBgAnimation, mWindmillScale, mWindmillRotate;
-
+    Animation mCircleBgAnimation, mWindmillScale, mWindmillRotate, mWindmillHide, mAdAnimation;
+    LinearLayout mAdLayout;
+    Rotate3dAnimation mRotate3dAnimation;
+    RelativeLayout mWindmillLayout;
+    boolean isOverturn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mCircleBg = (ImageView) findViewById(R.id.rotating_circle_bg);
         mWindmill = (ImageView) findViewById(R.id.rotating_windmill);
+        mWindmillLayout = (RelativeLayout) findViewById(R.id.windmill_layout);
+        mAdLayout = (LinearLayout) findViewById(R.id.ad_item);
         mCircleBgAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_background_show);
+        mWindmillLayout.setVisibility(View.VISIBLE);
         mCircleBg.setVisibility(View.VISIBLE);
         mCircleBg.startAnimation(mCircleBgAnimation);
-
         mWindmillScale = AnimationUtils.loadAnimation(this, R.anim.rotate_windmill_scale);
         mWindmillRotate = AnimationUtils.loadAnimation(this, R.anim.rotate_windill_rotate);
         mWindmillRotate.setInterpolator(new LinearInterpolator());
+        mWindmillHide = AnimationUtils.loadAnimation(this, R.anim.windmill_hide);
+        mAdAnimation = AnimationUtils.loadAnimation(this, R.anim.ad_show);
+        mAdAnimation.setInterpolator(new BounceInterpolator());
+
+        mRotate3dAnimation = new Rotate3dAnimation(Rotate3dAnimation.Rotate_X, 90, 0, mAdLayout.getWidth() / 2, mAdLayout.getHeight() / 2, 0, true);
+        mRotate3dAnimation.setFillAfter(true);
+        mRotate3dAnimation.setDuration(800);
+        mRotate3dAnimation.setInterpolator(new LinearInterpolator());
+        setListener();
+
+    }
+
+    private void setListener() {
         mCircleBgAnimation.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
@@ -70,15 +89,68 @@ public class RotateViewActivity extends BaseBackActivity {
 
             }
         });
+
+        mWindmillHide.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mWindmillLayout.setVisibility(View.GONE);
+                if (isOverturn) {
+                    mAdLayout.startAnimation(mRotate3dAnimation);
+                } else {
+                    mAdLayout.startAnimation(mAdAnimation);
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        mAdAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                mAdLayout.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mAdLayout.clearAnimation();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
+
     }
 
-    public void scale(View view) {
 
+    public void scale(View view) {
+        isOverturn = false;
+        mWindmill.clearAnimation();
+        mAdLayout.clearAnimation();
+        mAdLayout.setVisibility(View.INVISIBLE);
+        mWindmillLayout.setVisibility(View.VISIBLE);
+        mWindmillLayout.startAnimation(mWindmillHide);
     }
 
 
     public void overturn(View view) {
-
+        isOverturn = true;
+        mWindmill.clearAnimation();
+        mAdLayout.clearAnimation();
+        mAdLayout.setVisibility(View.INVISIBLE);
+        mWindmillLayout.setVisibility(View.VISIBLE);
+        mWindmillLayout.startAnimation(mWindmillHide);
     }
 
 
---
+++ app/src/main/java/com/animation/animation/util/Rotate3dAnimation.java
@@ -0,0 +1,77 @@
+package com.animation.animation.util;
+
+import android.graphics.Camera;
+import android.graphics.Matrix;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+
+/**
+ * Created by Jone on 17/4/21.
+ */
+
+public class Rotate3dAnimation extends Animation {
+
+    public static final int Rotate_X = 0;
+    public static final int Rotate_Y = 1;
+    public static final int Rotate_Z = 2;
+    Camera mCamera;
+    boolean mReverse;
+    private float mFromDegrees, mToDegrees, mPivotY, mPivotX, mPivotZ, mPreDegrees;
+    //默认绝对值坐标
+    private int mRotateDirection;
+
+    public Rotate3dAnimation(int rotateDirection, float fromDegrees, float toDegrees, float pivotX, float pivotY, float pivotZ, boolean reverse) {
+        mRotateDirection = rotateDirection;
+        mFromDegrees = fromDegrees;
+        mToDegrees = toDegrees;
+        mPivotY = pivotY;
+        mPivotX = pivotX;
+        mPivotZ = pivotZ;
+        mReverse = reverse;
+    }
+
+
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t) {
+        super.applyTransformation(interpolatedTime, t);
+
+        final float fromDegrees = mFromDegrees;
+        float currentDegrees = fromDegrees + (mToDegrees - fromDegrees) * interpolatedTime;
+        if(currentDegrees==mPreDegrees){
+            return;
+        }
+        System.out.println("=============刷新度数=====" + currentDegrees + "=======mFromDegrees===" + mFromDegrees + "====interpolatedTime===" + interpolatedTime);
+        Matrix matrix = t.getMatrix();
+        mCamera.save();
+        if (mReverse) {
+            mCamera.translate(0.0f, 0.0f, mPivotZ * interpolatedTime);
+        } else {
+            mCamera.translate(0.0f, 0.0f, mPivotZ * (1.0f - interpolatedTime));
+        }
+        switch (mRotateDirection) {
+            case Rotate_X:
+                mCamera.rotateX(currentDegrees);
+                break;
+            case Rotate_Y:
+                mCamera.rotateY(currentDegrees);
+                break;
+            case Rotate_Z:
+                mCamera.rotateZ(currentDegrees);
+                break;
+        }
+        mCamera.getMatrix(matrix);
+        mCamera.restore();
+        matrix.preTranslate(-mPivotX, -mPivotY);
+        matrix.postTranslate(mPivotX, mPivotY);
+        mPreDegrees = currentDegrees;
+    }
+
+
+    @Override
+    public void initialize(int width, int height, int parentWidth, int parentHeight) {
+        super.initialize(width, height, parentWidth, parentHeight);
+        mCamera = new Camera();
+        mPivotY = height / 2;
+        mPivotX = width / 2;
+    }
+}
